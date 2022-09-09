7cc40aac6367f8f3f8a383673fefe0af5ba8b18f
i-Martian <tabijulius@yahl>
2022-09-09 11:24:25 -0700

    commit of byte size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ainteger;
+	char acharacter;
+	float afloat;
+	long along;
+	long long alonglong;
+
+	printf("The Size of an int: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+
+	return (0);
+}
