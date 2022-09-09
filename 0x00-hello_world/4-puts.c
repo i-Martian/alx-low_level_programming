a96f6872bd92f42a2920b7e61202157dc03c3a3b
i-Martian <tabijulius@yahoo.com>
2022-09-09 14:16:50 +0100

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#include <stdio.h
+#include <stdio.h>
 
 int puts(){
-	a = "Programming is like building a multilingual puzzle/n"
-	
+	a="Programming is like building a multilingual puzzle/n"
 	return 0
 }
